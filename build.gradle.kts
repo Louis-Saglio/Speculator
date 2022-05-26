24a0add7c1b08a2232b96cc9ad3a8d159c8aa089
Louis Saglio <louis.saglio@proton.me>
2022-05-27 00:01:37 +0200

    Add required bus stations

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,6 +6,7 @@ plugins {
     application
     kotlin("jvm") version "1.6.21"
     id("com.github.johnrengelman.shadow") version "7.1.2"
+    kotlin("plugin.serialization") version "1.6.21"
 }
 
 group = "com.example"
@@ -25,7 +26,13 @@ repositories {
 dependencies {
     implementation("io.ktor:ktor-server-core-jvm:$ktor_version")
     implementation("io.ktor:ktor-server-cio-jvm:$ktor_version")
+    implementation("io.ktor:ktor-client-core:$ktor_version")
+    implementation("io.ktor:ktor-client-cio:$ktor_version")
     implementation("ch.qos.logback:logback-classic:$logback_version")
+    implementation("io.ktor:ktor-server-html-builder:$ktor_version")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.3")
+    implementation("io.ktor:ktor-client-content-negotiation:$ktor_version")
+    implementation("io.ktor:ktor-serialization-kotlinx-json:$ktor_version")
     testImplementation("io.ktor:ktor-server-tests-jvm:$ktor_version")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
 }
--- src/main/kotlin/com/example/Application.kt
+++ src/main/kotlin/com/example/Application.kt
@@ -1,15 +1,105 @@
 package com.example
 
-import com.example.plugins.configureRouting
+import io.ktor.client.*
+import io.ktor.client.call.*
+import io.ktor.client.plugins.*
+import io.ktor.client.plugins.contentnegotiation.*
+import io.ktor.client.request.*
+import io.ktor.http.*
+import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.application.*
 import io.ktor.server.cio.*
 import io.ktor.server.engine.*
+import io.ktor.server.html.*
+import io.ktor.server.routing.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.withContext
+import kotlinx.html.*
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.JsonNames
 
 fun main() {
     embeddedServer(CIO, port = 8081, host = "0.0.0.0") {
-        configureRouting()
         install(ShutDownUrl.ApplicationCallPlugin) {
             shutDownUrl = "/shutdown/"
         }
+        configureBusStopsEndpoint()
     }.start(wait = true)
+}
+
+val client = HttpClient {
+    install(ContentNegotiation) {
+        json(
+            Json {
+                prettyPrint = true
+                ignoreUnknownKeys = true
+            },
+            ContentType.Application.Json,
+        )
+    }
+    defaultRequest {
+        url {
+            protocol = URLProtocol.HTTPS
+            host = "ws.infotbm.com"
+            url("/ws/1.0/")
+        }
+    }
+}
+
+@Serializable
+data class BusArrival(@SerialName("waittime_text") val waitTimeText: String)
+
+@Serializable
+data class BusArrivalsResponsePayload0(@JsonNames("5315", "3647", "2947", "3270") val destinations: List<BusArrival>)
+
+@Serializable
+data class BusArrivalsResponsePayload(val destinations: BusArrivalsResponsePayload0)
+
+data class BusArrivalMetaData(val stopName: String, val line: String, val urlPath: String)
+
+val busStops = listOf(
+    BusArrivalMetaData("Eglise de Caudéran", "2", "get-realtime-pass/3169/02"),
+    BusArrivalMetaData("Eglise de Caudéran", "3", "get-realtime-pass/3169/03"),
+    BusArrivalMetaData("Quinconces", "2", "get-realtime-pass/3648/02"),
+    BusArrivalMetaData("Quinconces", "3", "get-realtime-pass/3648/03"),
+)
+
+suspend fun getNextBusArrival(data: BusArrivalMetaData) = withContext(Dispatchers.IO) {
+    client.get(data.urlPath).body<BusArrivalsResponsePayload>().destinations.destinations
+}
+
+fun Application.configureBusStopsEndpoint() {
+    routing {
+        get("/bus") {
+            val result = async {
+                busStops.associateWith { getNextBusArrival(it) }
+            }
+            val nextBusArrivalsByStation = result.await()
+            call.respondHtml {
+                head {
+                    title {
+                        +"TBM - Bus"
+                    }
+                }
+                body {
+                    h1 {
+                        +"Prochains bus"
+                    }
+                    nextBusArrivalsByStation.forEach { (station, nextBusArrivals) ->
+                        h2 {
+                            +"${station.stopName} - ${station.line}"
+                        }
+                        ol {
+                            nextBusArrivals.forEach {
+                                li { +it.waitTimeText }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
